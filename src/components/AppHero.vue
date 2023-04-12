9fe5b2a6edbc5b80f08e2d452620a3415c739029
Franz2610 <francescofroechlich1@gmail.com>
2023-04-12 16:13:58 +0200

    .

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="jumbo text-center align-content-center">
-   <h3>{{ jumbo }}</h3> 
-   <img :src="image" alt="vite">
-    
+    <div class="jumbo text-center align-content-center bg-success text-white mt-5">
+        <img :src="image" alt="vite">
+       <h1>{{ icon }}</h1> 
+        <img class="mb-2" :src="image2" alt="vite">
     </div>
+    <div class="text-center">
+        <h2>{{ jumbo }}</h2>
+    </div>
+
 </template>
 
 <script>
@@ -11,8 +15,10 @@
         name:'AppHero',
         data(){
             return{
-                jumbo:'Jumbo!',
-                image :'https://vitejs.dev/logo-with-shadow.png'
+                jumbo:'La fusione tra Vite e Vue',
+                image :'https://vitejs.dev/logo-with-shadow.png',
+                image2: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
+                icon: '+'
             }
         }
         
