37984ab2d7877adc1ddd7e060990dcc6d62b33e0
Franz2610 <francescofroechlich1@gmail.com>
2023-04-12 14:22:35 +0200

    vite-hello-world

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+:<template>
+    <header>
+    {{ jumbo }}
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'AppHero',
+        data(){
+            return{
+                jumbo:'La mia prima app con Vite!'
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+:<template>
+    <header>
+    {{ titolo }}
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'AppTitle',
+        data(){
+            return{
+                titolo:'La mia prima app con Vite!'
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
